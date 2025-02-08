c7ffe04a0ec01d43fbebc29215ec8fb2559e99e0
Smit Patel <smit.r.patel5@gmail.com>
2025-02-08 11:47:34 -0500

    Added User Logout and Restricted Pages

--- base/views.py
+++ base/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
-from django.contrib.auth.models import User
+
 
 
 '''
@@ -25,10 +28,21 @@ def loginPage(request):
         except:
             messages.error(request, "User does not exist")
 
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
         
+        else:
+            messages.error(request, 'Username OR password does not exist')
 
     context = {}
     return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -51,7 +65,7 @@ def room(request, pk):
     context  = {'room':room}
     return render(request, 'base/room.html', context)
 
-
+@login_required(login_url='/login')
 def createRoom(request):
     form = RoomForm()
     if request.method == "POST":
